2e300a917105fe39108e5aba20fa0fdc9adae3d3
Emmanuel Hope <swingscruzehope@gmail.com>
2025-07-30 10:27:24 +0100

    Add files via upload

--- apiapp/urls.py
+++ apiapp/urls.py
@@ -6,10 +6,11 @@ from . import views
 urlpatterns = [
 
 
-    path("", views.home),
-    path("home/", views.home),
-    path("adduser/", views.adduser),
-    path("deleteuser/", views.deleteuser),
+    path("", views.attackareal),
+    path("home/", views.attackareal),
+    path("alluser/", views.UserView.as_view()),
+    path("removeuser/<int:id>/", views.RemoveUser.as_view()),
+    path("user/<int:id>/", views.FetchUser.as_view()),
 ]
 
 
--- apiapp/views.py
+++ apiapp/views.py
@@ -1,21 +1,59 @@
 from django.shortcuts import render, HttpResponse
+from rest_framework.views import APIView, status
+from rest_framework.response import Response
+from .models import user
+from .serializers import ItemSerializer
 
 # Create your views here.
 
 
 
-def home(request):
-    return HttpResponse("hello welcome home")
 
 
-def deleteuser(request):
 
-    return HttpResponse("time to delet user")
+def attackareal(request):
+
+    return render(request, "attack_template.html")
 
 
-def adduser(request):
+class UserView(APIView):
 
-    return HttpResponse("time to adduser ")
+    def get(self, request):
+        items = user.objects.all()
+        serializer = ItemSerializer(items, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ItemSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+  
+
+class RemoveUser(APIView):
+
+    def get(self, request, id):
+        items = user.objects.get(id=id)
+        if items:
+            items.delete()
+
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+class FetchUser(APIView):
+
+    def get(self, request, id):
+        userinfor = user.objects.get(id=id)
+
+        serializedata = ItemSerializer(userinfor)
+
+        return Response(serializedata.data, status= status.HTTP_200_OK)
+
 
 
 
